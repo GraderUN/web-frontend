fe8073f1a86182e8c03403265d0b58b514496510
jcvar <36166613+jcvar@users.noreply.github.com>
2020-10-19 03:08:30 -0500

    vue add apollo

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import router from './router'
 
 import '@/icons' // icon
 import '@/permission' // permission control
+import { createProvider } from './vue-apollo'
 
 // import { createProvider } from './vue-apollo';
 import { ApolloClient } from 'apollo-client'
@@ -64,5 +65,6 @@ new Vue({
   apolloProvider,
   router,
   store,
+  apolloProvider: createProvider(),
   render: h => h(App)
 })
