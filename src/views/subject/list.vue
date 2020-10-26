431d586dfb5e00fddbcf125c526dd6f02f8ad3d1
jcvar <36166613+jcvar@users.noreply.github.com>
2020-10-26 02:48:04 -0500

    Edit dialog in subject list

--- src/views/subject/list.vue
+++ src/views/subject/list.vue
@@ -1,10 +1,66 @@
 <template>
   <div class="app-container">
-    <el-table :data="getSubjects" border>
-      <el-table-column prop="id" label="ID" width="50px"> </el-table-column>
-      <el-table-column prop="name" label="Name" sortable> </el-table-column>
-      <el-table-column prop="grade" label="Grade" sortable> </el-table-column>
+    <el-button
+      type="primary"
+      round
+      size="medium"
+      icon="el-icon-refresh"
+      @click="updateList"
+    >
+      Update
+    </el-button>
+    <el-table
+      height="500"
+      border
+      :data="
+        getSubjects.filter(
+          (data) =>
+            !search || data.name.toLowerCase().includes(search.toLowerCase())
+        )
+      "
+    >
+      <el-table-column prop="id" label="ID" width="50px" />
+      <el-table-column prop="name" label="Name" sortable />
+      <el-table-column prop="grade" label="Grade" sortable />
+      <el-table-column align="right">
+        <!-- eslint-disable-next-line -->
+        <template slot="header" slot-scope="scope">
+          <el-input v-model="search" size="mini" placeholder="Type to search" />
+        </template>
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="warning"
+            plain
+            @click="handleEdit(scope.row)"
+          >
+            Edit
+          </el-button>
+        </template>
+      </el-table-column>
     </el-table>
+
+    <el-dialog :visible.sync="showDialog">
+      <span slot="title">Editing subject (id: {{ form.id }})</span>
+      <el-form :model="form">
+        <el-form-item label="Subject name">
+          <el-input v-model="form.name" />
+        </el-form-item>
+
+        <el-form-item label="Subject grade">
+          <el-input-number
+            v-model="form.grade"
+            :min="-1"
+            :max="11"
+            controls-position="right"
+          />
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="danger" @click="handleDelete">Delete</el-button>
+        <el-button type="warning" @click="handleUpdate">Update</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -14,6 +70,9 @@ import gql from 'graphql-tag'
 export default {
   data() {
     return {
+      search: '',
+      showDialog: false,
+      id: 0,
       form: {
         name: '',
         grade: 0
@@ -31,7 +90,27 @@ export default {
       }
     `
   },
-  // methods: {}
+  methods: {
+    updateList() {
+      this.$apollo.queries.getSubjects.refetch().then(() => {
+        this.$message({
+          message: 'Subject list updated!',
+          type: 'success'
+        })
+      }
+      ).catch(() => {
+        this.$message.error('Error updating Subjects')
+      })
+    },
+    handleEdit(r) {
+      this.showDialog = true
+      this.form.id = r.id
+      this.form.name = r.name
+      this.form.grade = r.grade
+    },
+    handleDelete() { },
+    handleUpdate() { }
+  }
 }
 </script>
 
