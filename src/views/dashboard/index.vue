f37ba9b2622664da7c4a3897d484dc40d43b1f23
jcvar <36166613+jcvar@users.noreply.github.com>
2020-11-24 00:41:03 -0500

    Add rankings to dashboad

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,20 +1,64 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
+
+    <div class="app-container">
+      <el-button
+        type="primary"
+        round
+        size="medium"
+        icon="el-icon-refresh"
+        @click="updateList"
+      >
+        Update
+      </el-button>
+      <el-table border :data="allPuntajes">
+        <el-table-column type="index" label="#" width="50" />
+        <el-table-column prop="name" label="Name" />
+        <el-table-column prop="email" label="Email" />
+        <el-table-column prop="puntaje" label="Score" width="100px" sortable />
+      </el-table>
+
+    </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
+import gql from "graphql-tag";
 
 export default {
-  name: 'Dashboard',
+  name: "Dashboard",
   computed: {
-    ...mapGetters([
-      'name'
-    ])
-  }
-}
+    ...mapGetters(["name"]),
+  },
+  apollo: {
+    allPuntajes: gql`
+      query {
+        allPuntajes {
+          name
+          email
+          puntaje
+        }
+      }
+    `,
+  },
+  methods: {
+    updateList() {
+      this.$apollo.queries.allPuntajes
+        .refetch()
+        .then(() => {
+          this.$message({
+            message: "Ranking list updated!",
+            type: "success",
+          });
+        })
+        .catch(() => {
+          this.$message.error("Error updating Ranking");
+        });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
