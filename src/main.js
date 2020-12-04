f1d66de8ae79dbb60e50cb0024ea46847eaa69c4
samoralespu <samoralespu@unal.edu.co>
2020-12-04 17:08:08 -0500

    Not working proof 2

--- src/main.js
+++ src/main.js
@@ -49,9 +49,9 @@ Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
 
-const GATEWAY = getEnv('VUE_APP_VARIABLE')
+const GATEWAY = getEnv('VUE_APP_GATEWAY')
 console.log('gateway:')
-console.log(GATEWAY)
+console.log(getEnv('VUE_APP_VARIABLE'))
 console.log('variable1:')
 console.log(getEnv('VUE_APP_VARIABLE_1'))
 const httpLink = new HttpLink({
